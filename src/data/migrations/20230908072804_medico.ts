946239a3d6a7418a134a86391d1d1788bfff60ae
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-09 04:31:02 -0300

    adiciona coluna senha na entidade paciente e médico

--- src/data/migrations/20230908072804_medico.ts
+++ src/data/migrations/20230908072804_medico.ts
@@ -6,6 +6,7 @@ export async function up(knex: Knex): Promise<void> {
     table.string('nome').notNullable();
     table.string('cpf').unique().notNullable();
     table.date('data_nascimento').notNullable();
+    table.string('senha').notNullable();
   });
 }
 
