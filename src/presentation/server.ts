0088880a6fc673fb4cd3609db3e4e756d7fb60ba
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-10 03:34:17 -0300

    muda o diretório de server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { pacienteRouter } from './rest/paciente/paciente-routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(pacienteRouter);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`Server started on port ${port}`));
