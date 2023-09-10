b830c7b40c3ee014a2af8c631305b78c35b346a8
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-10 03:23:26 -0300

    adiciona paciente repository

---
+++ src/data/repositories/interfaces/paciente.ts
@@ -0,0 +1,6 @@
+import { IPaciente } from '../../../domain/entities/interfaces/paciente';
+
+export interface IPacienteRepository {
+  create(paciente: IPaciente): Promise<IPaciente>;
+  findByCPF(id: string): Promise<IPaciente>;
+}
---
+++ src/data/repositories/paciente-repository.ts
@@ -0,0 +1,22 @@
+import Knex from 'knex';
+import config from '../../../knexfile';
+import { IPaciente } from '../../domain/entities/interfaces/paciente';
+import { IPacienteRepository } from './interfaces/paciente';
+
+const knex = Knex(config);
+
+export class PacienteRepository implements IPacienteRepository {
+  async create(paciente: Omit<IPaciente, 'id'>): Promise<IPaciente> {
+    const [createdPaciente] = await knex<IPaciente>('pacientes')
+      .insert(paciente)
+      .returning('*');
+    return createdPaciente;
+  }
+
+  async findByCPF(cpf: string): Promise<IPaciente> {
+    const [paciente] = await knex<IPaciente>('pacientes')
+      .select('*')
+      .where({ cpf });
+    return paciente;
+  }
+}
